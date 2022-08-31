da49edb31bf5f66d7fd26629a05cd805ac930c42
Selcuk Ufuk <selcukkrds09@gmail.com>
2022-08-31 09:33:28 +0200

    31.09

---
+++ First_Class.java
@@ -0,0 +1,61 @@
+public class First_Class {
+
+    public static void main(String[] args) {
+        System.out.println("Tell me about yourself? (Kendinden bahsedermisin?)");
+        System.out.println("Sadece bir cumle");
+        /*
+        Merhaba arkadaslar ,er kisi icin 5 satir belirlendi lutfen belirlenen
+        satirin disina cikmamamya dikkat edelim.
+        - Yusuf bey -->16-20          - Filiz Hanim --> 41-45
+        - Nihat bey --> 21-25         - Fatih Ok bey --> 46-50
+        - Seray hanim --> 26-30       - Yakub bey --> 51-55
+        - Fatih Kurkcu bey--> 31-35   - Selcuk --> 56-60
+        - Talha bey --> 36-40
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Selcuk
+        System.out.println("Adim Selcuk. ");
+
+
+    }
+}
